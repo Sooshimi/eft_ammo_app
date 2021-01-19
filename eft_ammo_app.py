f6e93de90867165809a04a661211812be440d126
Sooshimi <masonchee93@gmail.com>
2021-01-19 11:44:56 +0000

    update

--- eft_ammo_app.py
+++ eft_ammo_app.py
@@ -3,13 +3,6 @@ import matplotlib.pyplot as plt
 import pymongo
 import seaborn as sb
 import numpy as np
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
-from kivy.lang import Builder
 
 pd.set_option('display.max_columns', None)
 ammo_df = pd.read_csv("ammo.csv")
@@ -20,14 +13,4 @@ size_df = ammo_df["Size"].unique()
 #filtering by size
 ammo_df[ammo_df["Size"] == "12 Gauge Shot"]
 
-########################## KIVY GUI #################################
-
-class MyGrid(Widget):
-    pass
-
-class MyApp(App):
-    def build(self):
-        return MyGrid()
-
-if __name__ == '__main__':
-    MyApp().run()
+print(ammo_df.head())
